60d4dded24f9cf2bacdd14ba107eed0c06c503f1
ekdromoi <warbornleonidas@gmail.com>
2026-05-19 20:01:45 +0530

    fix ui

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -27,7 +27,7 @@ export function formatDateTime(dateStr: string | Date): string {
   return (
     d.toLocaleDateString(undefined, { month: "short", day: "numeric", year: "numeric" }) +
     ", " +
-    d.toLocaleTimeString(undefined, { hour: "2-digit", minute: "2-digit" })
+    d.toLocaleTimeString(undefined, { hour: "2-digit", minute: "2-digit", hour12: true })
   );
 }
 
